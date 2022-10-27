7512d01180a44e11696fb3ed5819d450484f07ba
Rede Alexander <redealexander9@gmail.com>
2022-10-27 18:29:25 -0500

    Added dialogue box for creating a playlist. Added A text box and buttons to it, not implemented

---
+++ newPlaylistWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace myTunes
+{
+    /// <summary>
+    /// Interaction logic for newPlaylistWindow.xaml
+    /// </summary>
+    public partial class newPlaylistWindow : Window
+    {
+        public newPlaylistWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
